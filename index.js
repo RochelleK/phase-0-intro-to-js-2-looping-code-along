1450cb16de85eeba54c4c085e365949001669dae
Rochelle Katzman <zelrochelle@gmail.com>
2022-11-08 16:46:47 -0500

    commit tree 50bf0d6fc52b8ced6af6478d8cf995266dae38e9

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const newarr = []; 
+
+function writeCards(arr, event){
+    for (let i = 0; i < arr.length; i++){
+        newarr[i] = (`Thank you, ${arr[i]}, for the wonderful ${event} gift!`); 
+    } 
+    return newarr; 
+}
+
+writeCards(); 
+
+function countDown(a){
+    for (let i = a; i >= 0; i--){
+        console.log(i)
+    }
+}
+
+countDown(); 
